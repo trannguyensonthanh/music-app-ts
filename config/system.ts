b85dc23a4fabee3f7cad35c0ae1510710a36a47b
Trần Nguyễn Sơn Thành <sonthanh12345678910@gmail.com>
2024-01-09 10:45:52 +0700

    kết nối database

---
+++ config/system.ts
@@ -0,0 +1,5 @@
+const PATH_ADMIN = "/admin";
+
+module.exports = {
+    prefixAdmin : PATH_ADMIN  // có thể định nghĩa tên cho module theo cách này
+};
--- index.ts
+++ index.ts
@@ -1,6 +1,6 @@
 import express, {Express, Request, Response} from "express"
 import dotenv from "dotenv";
-// import * as database from "./config/database";
+import * as database from "./config/database";
 // import cors from 'cors'
 // import  routeApiVer1  from "./api/v1/routes/index.route";
 // import bodyParser from 'body-parser'
@@ -10,7 +10,7 @@ dotenv.config();
 
 app.set("views", `${__dirname}/views`); // đẩy dữ liệu ra views  sử dụng thêm __dirname để sử dụng trên cả online luôn
 app.set("view engine", "pug"); // sử dụng pug
-
+database.connect(); // kết nối với mongodb
 app.get("/topics", async (req: Request, res: Response) => {
  res.render("client/pages/topics/index.pug");
 }) 

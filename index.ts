7bd9cb1752b4f88c6961da1b0591411b1fceb523
Trần Nguyễn Sơn Thành <sonthanh12345678910@gmail.com>
2024-01-09 10:36:34 +0700

    cài đặt pug

--- index.ts
+++ index.ts
@@ -8,9 +8,12 @@ const app: Express = express();
 const port: number | string = process.env.PORT || 3000
 dotenv.config();
 
+app.set("views", `${__dirname}/views`); // đẩy dữ liệu ra views  sử dụng thêm __dirname để sử dụng trên cả online luôn
+app.set("view engine", "pug"); // sử dụng pug
+
 app.get("/topics", async (req: Request, res: Response) => {
-  res.send("chủ đề bài hát");
-})
+ res.render("client/pages/topics/index.pug");
+}) 
 
 app.listen(port, () => {
   console.log(`Server is running at http://localhost:${port}`);
